2246f3ee473e56760251318db1d4613bace694b2
KDPG13 <104178566+KDPG13@users.noreply.github.com>
2022-09-22 18:01:24 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const allNotes = require('./db/db.json');
+const PORT = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+const apiRoutes = require("./routes/apiRoutes");
+app.use(apiRoutes);
+const htmlRoutes = require("./routes/htmlRoutes");
+app.use(htmlRoutes);
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
